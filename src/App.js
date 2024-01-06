3b6397b4c8a60868352d3cc9368d6c0604669693
bsj039190 <bsj039190.gmail.com>
2024-01-06 14:42:29 +0900

    240106 labtop commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
